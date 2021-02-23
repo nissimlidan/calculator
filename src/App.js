d22cb95dd0241c489696069e8f0f8e101556e555
Lidan Nissim <nissimlidan@gmail.com>
2021-02-23 14:50:21 +0200

    React calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ Component } from "react";
+import Button from './components/Button';
+import './css/style.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props){
+      super(props);
+
+      this.state = {
+
+        current: "0", 
+        priveous: [],
+        nextIsReset: false,
+      }
+  }
+
+  reset = () => {
+    this.setState({current: '0', priveous: [], nextIsReset: false});
+  }
+
+  addToCurrent = (symbol) => {
+    if(["/", "x", "+", "-"].indexOf(symbol) > -1){
+      let {priveous} = this.state;
+      priveous.push(this.state.current + symbol);
+      this.setState({priveous, nextIsReset: true});
+
+    }else {
+      if((this.state.current === "0" && symbol !== ".") || this.state.nextIsReset)
+      this.setState({ current: symbol, nextIsReset: false});
+      else{
+        this.setState({ current: this.state.current + symbol});
+      }
+    }
+    
+  }
+
+  calculate = (symbol) => {
+    let {current , priveous, nextIsReset} = this.state;
+    if(priveous.length > 0 ){
+      current = eval(String(priveous[priveous.length - 1] + current));
+      this.setState({current , priveous: [], nextIsReset: true});
+    }
+
+  }
+
+  render(){
+
+    const buttons = [
+
+      {symbol: 'C', cols: 2, action: this.reset },
+      {symbol: 'Del', cols: 1, action: this.reset },
+      {symbol: '/', cols: 1, action: this.addToCurrent },
+      {symbol: '1', cols: 1, action: this.addToCurrent },
+      {symbol: '2', cols: 1, action: this.addToCurrent },
+      {symbol: '3', cols: 1, action: this.addToCurrent },
+      {symbol: '*', cols: 1, action: this.addToCurrent },
+      {symbol: '4', cols: 1, action: this.addToCurrent },
+      {symbol: '5', cols: 1, action: this.addToCurrent },
+      {symbol: '6', cols: 1, action: this.addToCurrent },
+      {symbol: '+', cols: 1, action: this.addToCurrent },
+      {symbol: '7', cols: 1, action: this.addToCurrent },
+      {symbol: '8', cols: 1, action: this.addToCurrent },
+      {symbol: '9', cols: 1, action: this.addToCurrent },
+      {symbol: '-', cols: 1, action: this.addToCurrent },
+      {symbol: '0', cols: 1, action: this.addToCurrent },
+      {symbol: '=', cols: 2, action: this.calculate},
+      {symbol: '.', cols: 1, action: this.addToCurrent },
+      
+    ]
+      return ( 
+      <div className="App">
+
+          {this.state.priveous.length > 0 ? <div className="floaty-last"> {this.state.priveous[this.state.priveous.length - 1]} </div> : null }
+
+          <input className="result" type="text" value={this.state.current}/>
+
+          { buttons.map((btn,i)=> {
+            return <Button key={i} symbol={btn.symbol} cols={btn.cols} action={(symbol) => btn.action(symbol)} />
+          })}
+ 
+      </div>
+
+
+      )
+
+    } 
 }
+
 
 export default App;
---
+++ src/components/Button.js
@@ -0,0 +1,19 @@
+import { render } from '@testing-library/react';
+import React, {Component} from 'react';
+
+class Button extends Component {
+
+    render() {
+
+        return(
+            <div className= {`column-${this.props.cols}`}>
+
+                <button className="calc-button" onClick={() => this.props.action(this.props.symbol)}> {this.props.symbol}</button>
+
+            </div>
+            
+        )
+    }
+}
+
+export default Button;
